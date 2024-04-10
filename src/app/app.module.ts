9109038d2742b6574084cad04790ae99e8b02fd2
Ezhilan-V <vezhilan24@gmail.com>
2024-04-10 07:47:52 -0700

    updated new angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,18 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { MaterialModule } from './material.module';
-import { ComponentsModule } from './components/components.module';
-import { ProfileModule } from './profile/profile.module';
-import { ProfileComponent } from './components/profile/profile.component';
+
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        ProfileComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-        ComponentsModule,
-        MaterialModule,
-        ProfileModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
