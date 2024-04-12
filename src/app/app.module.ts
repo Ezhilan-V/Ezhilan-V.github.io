6fb6bcbf581a7af47e33f788f7c37ffa6bda7880
Ezhilan-V <vezhilan24@gmail.com>
2024-04-11 19:03:47 -0700

    added old website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MaterialModule } from './material.module';
+import { ComponentsModule } from './components/components.module';
+import { ProfileModule } from './profile/profile.module';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        ProfileComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        // NgbModule,
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        ComponentsModule,
+        MaterialModule,
+        ProfileModule,
+        NgbModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
