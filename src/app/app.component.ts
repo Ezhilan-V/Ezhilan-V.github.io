23b747816268c36644b7631a02b9f3efba117416
Ezhilan-V <vezhilan24@gmail.com>
2024-08-03 18:31:47 -0700

    added new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {
-    }
+export class AppComponent {
+  title = 'Portfolio';
 }
