7f692ba134a3cb4b32a0de7fb8912e695a8a7889
Ezhilan-V <vezhilan24@gmail.com>
2024-11-16 18:32:43 -0700

    Revert "updated content for masters"
    This reverts commit 50a82a16b1df5b7dba50332f7e3fce946499d902.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,7 +8,6 @@ import { AgmCoreModule } from '@agm/core';
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
 import { ProjectCardComponent } from './project-card/project-card.component';
-import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
     exports:[ ProjectCardComponent],
@@ -26,8 +25,7 @@ import { ProjectsComponent } from './projects/projects.component';
         })
     ],
     declarations: [
-        ProjectCardComponent,
-        ProjectsComponent
+        ProjectCardComponent
     ]
 })
 export class ComponentsModule { }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from '../../../node_modules/ngx-spinner';
-import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,25 +7,20 @@ import { ProfileService } from './profile.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ResumeComponent implements OnInit {
-  about: string
-  constructor(private profileService: ProfileService) {
-    this.about = this.profileService.about
 
-  }
-
-
+  constructor(  ) { }
   ngOnInit() {
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('profile-page');
     var navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.add('navbar-transparent');
-  }
+      }
   ngOnDestroy() {
     var body = document.getElementsByTagName('body')[0];
     body.classList.remove('profile-page');
     var navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.remove('navbar-transparent');
   }
-
+ 
 
 }
